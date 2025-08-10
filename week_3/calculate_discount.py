ef35d5e4bf86bb5e8bf8a3e8af6f8bd81b88f691
Will Tsotetsi <will.tsotetsi@hotmail.com>
2025-08-10 02:39:32 +0200

    Add corrected calculate_discount.py file

--- week_3/calculate_discount.py
+++ week_3/calculate_discount.py
@@ -1,18 +1,21 @@
 
 
 def calculate_discount(price, discount_percent):
-    if discount_percent < 20:
+    if discount_percent >= 20:
+        discount_amount = price * (discount_percent / 100)
+        final_price = price - discount_amount
+        return final_price
+    else:
         return price
     
-    discount_amount = (discount_percent / 100) * price
-    final_price = price - discount_amount
-    return final_price
+price = float(input("Enter the original price of the item: "))
+discount_percent = float(input("Enter the discount percentage: "))
+final_price = calculate_discount(price, discount_percent)
 
-print(calculate_discount(100, 20))
-print(calculate_discount(100, 15))
-print(calculate_discount(100, 40))
-print(calculate_discount(100, 10))
-
+if final_price < price:
+    print(f"Discount applied! Final price is: ${final_price:.2f}")
+else:
+    print(f"No discount applied. Final price is: ${final_price:.2f}")
 
 
 
